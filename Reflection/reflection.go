c84067db9df8250ca3088a078910f45e635e328f
Aniket Bhardwaj <anicse37@gmail.com>
2025-04-15 13:25:01 +0530

    Reflection Complete

--- Reflection/reflection.go
+++ Reflection/reflection.go
@@ -1,20 +1,34 @@
 package reflection
 
 import (
+	"fmt"
 	"reflect"
 )
 
 func Walk(x interface{}, fn func(input string)) {
-
-	val := reflect.ValueOf(x)
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			Walk(field.Interface(), fn)
+	val := GetValue(x)
+	switch val.Kind() {
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			Walk(val.Index(i).Interface(), fn)
 		}
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			Walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.String:
+		// fmt.Println(val)
+		fn(val.String())
+	case reflect.Int:
+		// fmt.Println(val)
+		fn(fmt.Sprint(val.Int()))
 	}
+
+}
+func GetValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	return val
 }
--- Reflection/reflection_test.go
+++ Reflection/reflection_test.go
@@ -17,24 +17,33 @@ func TestWalk(t *testing.T) {
 			"struct with one string field",
 			struct {
 				Name string
-				City string
-			}{"Aniket", "Mohali"},
-			[]string{"Aniket", "Mohali"},
+			}{"Aniket"},
+			[]string{"Aniket"},
 		},
 		{
 			"struct with one string field",
 			struct {
 				Name string
-			}{"Aniket"},
-			[]string{"Aniket"},
+				City string
+			}{"Aniket", "Mohali"},
+			[]string{"Aniket", "Mohali"},
 		},
 		{
-			"struct with int field",
-			struct {
+			"struct with slice field",
+			[]struct { //struct
 				Name string
 				Age  int
-			}{"Aniket", 23},
-			[]string{"Aniket"},
+			}{
+				{"Aniket", 23}, //slice and int
+				{"Gtech", 25},
+				{"QWE", 24},
+			},
+			[]string{"Aniket", "23", "Gtech", "25", "QWE", "24"},
+		},
+		{
+			"struct with one string field",
+			2,
+			[]string{"2"},
 		},
 	}
 	for _, test := range cases {
