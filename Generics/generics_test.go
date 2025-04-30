d89397ac7f075c7793d52c970650530ff9c27913
Aniket Bhardwaj <anicse37@gmail.com>
2025-04-30 16:13:00 +0530

    Progress in Generics

--- Generics/generics_test.go
+++ Generics/generics_test.go
@@ -1,7 +1,12 @@
 package generics_test
 
-import "testing"
+import (
+	"testing"
 
+	generics "github.com/anicse37/TDD_Go/Generics"
+)
+
+/*-------------------------------------------------------*/
 func TestAssertFunctions(t *testing.T) {
 	t.Run("asserting on integers", func(t *testing.T) {
 		AssertEqual(t, 1, 1)
@@ -12,6 +17,43 @@ func TestAssertFunctions(t *testing.T) {
 		AssertNotEqual(t, "Hello", "World")
 	})
 }
+
+/*-------------------------------------------------------*/
+func TestStack(t *testing.T) {
+	t.Run("integer stack", func(t *testing.T) {
+		myStackOfInts := new(generics.StackOfInts)
+
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(19)
+		AssertFalse(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(84)
+		value, _ := myStackOfInts.Pop()
+		AssertEqual(t, value, 84)
+		value, _ = myStackOfInts.Pop()
+		AssertEqual(t, value, 19)
+		AssertTrue(t, myStackOfInts.IsEmpty())
+	})
+
+	t.Run("Stack of Strings", func(t *testing.T) {
+		myStackOfStrings := new(generics.StackOfStrings)
+
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+
+		myStackOfStrings.Push("19")
+		AssertFalse(t, myStackOfStrings.IsEmpty())
+
+		myStackOfStrings.Push("84")
+		value, _ := myStackOfStrings.Pop()
+		AssertEqual(t, value, "84")
+		value, _ = myStackOfStrings.Pop()
+		AssertEqual(t, value, "19")
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+	})
+}
+
+/*-------------------------------------------------------*/
 func AssertEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got != want {
@@ -24,3 +66,19 @@ func AssertNotEqual[T comparable](t *testing.T, got, want T) {
 		t.Error("Do not want: ", got)
 	}
 }
+
+/*-------------------------------------------------------*/
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Errorf("g=Got %v || Want True", got)
+	}
+}
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Errorf("g=Got %v || Want True", got)
+	}
+}
+
+/*-------------------------------------------------------*/
